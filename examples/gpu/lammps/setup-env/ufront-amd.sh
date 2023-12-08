4c52c8478ab75d7912eb8a4c2a703c8113deb278
Srđan Milaković <sm108@rice.edu>
2023-12-08 02:10:23 -0600

    Fixed LAMMPS for ufront-amd.sh

--- examples/gpu/lammps/setup-env/ufront-amd.sh
+++ examples/gpu/lammps/setup-env/ufront-amd.sh
@@ -1,11 +1,11 @@
 # load modules needed to build and run lammps
 module use /projects/modulefiles
 module use /usr/local/modules
-module load rocm/5.5.0-devel cmake
+module load rocm/5.4.3 cmake
 
 # modules for hpctoolkit
 module use /home/johnmc/modulefiles
-export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/default"
+export HPCTOOLKIT_MODULES_HPCTOOLKIT="module load hpctoolkit/2023.08.1"
 $HPCTOOLKIT_MODULES_HPCTOOLKIT
 
 # environment settings for this example
@@ -16,7 +16,7 @@ export HPCTOOLKIT_LAMMPS_GPU_ARCH="-DKokkos_ARCH_VEGA90A=ON"
 export HPCTOOLKIT_LAMMPS_HOST_ARCH="-DKokkos_ARCH_ZEN2=ON"
 export HPCTOOLKIT_LAMMPS_GPUFLAGS="-DKokkos_ENABLE_HIP=yes -DCMAKE_CXX_COMPILER=$(which hipcc) -DCMAKE_CXX_FLAGS=\"-g\""
 export HPCTOOLKIT_LAMMPS_SUBMIT="sh"
-export HPCTOOLKIT_LAMMPS_RUN="env ROCR_VISIBLE_DEVICES=3,4 sh"
+export HPCTOOLKIT_LAMMPS_RUN="env ROCR_VISIBLE_DEVICES=1,2 sh"
 export HPCTOOLKIT_LAMMPS_RUN_PC="sh make-scripts/unsupported-amd.sh"
 export HPCTOOLKIT_LAMMPS_BUILD="sh"
 export HPCTOOLKIT_LAMMPS_OMP_NUM_THREADS=1
